5ccbb7ab9222a37d8601ecf19ca177b13406df78
Jonathan Lee <Jonathan@macmini-wifi.omghax.biz>
2014-09-28 22:05:34 -0700

    add time variable

--- js/app.js
+++ js/app.js
@@ -13,12 +13,36 @@ $(document).ready( function() {
 				$('.yourhigh').text(data.daily.apparentTemperatureMax);
 				$('.yourlow').text(data.daily.apparentTemperatureMin);				
 				console.log(data.currently.temperature);
-				console.log(data.currently.apparentTemperatureMin);
-				console.log(data.currently.apparentTemperatureMax);
+				console.log(data.daily.apparentTemperatureMin);
+				console.log(data.daily.apparentTemperatureMax);
 			},
 			error: function(){
 				console.log(arguments[1]);
 			}
 		});
 	});
+
+	navigator.geolocation.getCurrentPosition(function(location) {
+		var lat = location.coords.latitude,
+			long = location.coords.longitude,
+			time = function() {
+				$.now();
+				return time;
+			},
+			url = 'https://api.forecast.io/forecast/5e9457d35ada3f7d120f34384e7f173f/' + lat + ',' + long + ',' + time; 
+
+		$.ajax({
+			url: url,
+			type: 'GET',
+			dataType: 'jsonp',
+			success: function(data) {
+				$('.yourhigh').text(data.daily.apparentTemperatureMax);
+				console.log(data.daily.apparentTemperatureMin);
+			},
+			error: function(){
+				console.log(arguments[1]);
+			}
+		});
+	});
+
 });
