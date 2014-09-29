dc75d2c28101e035eb1424e83fe88f9fff49bbd6
Jonathan Lee <Jonathan@macmini-wifi.omghax.biz>
2014-09-28 21:45:24 -0700

    swap cities

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ $(document).ready( function() {
 			type: 'GET',
 			dataType: 'jsonp',
 			success: function(data) {
-				$('.sandiegotemp').text(data.currently.temperature);
-				$('.sdhigh').text(data.daily.apparentTemperatureMax);
-				$('.sdlow').text(data.daily.apparentTemperatureMin);				
+				$('.yourcitytemp').text(data.currently.temperature);
+				$('.yourhigh').text(data.daily.apparentTemperatureMax);
+				$('.yourlow').text(data.daily.apparentTemperatureMin);				
 				console.log(data.currently.temperature);
 				console.log(data.currently.apparentTemperatureMin);
 				console.log(data.currently.apparentTemperatureMax);
