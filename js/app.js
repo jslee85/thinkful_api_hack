2f953bcee129cf56122b70e73ca9bee3890fc6d6
Jonathan Lee <Jonathan@macmini-wifi.omghax.biz>
2014-09-28 21:34:11 -0700

    failed attempts at AJAX

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
-$(document).ready( function() {
+var yourWeather = function() {
+	$('.yourcitytemp').text(getNow());
+	$('.yourhigh').text(getNow());
+	$('.yourlow').text(getNow());
+};
 
-});
+var sdWeather = function() {
+	$('.sandiegotemp').text(getNow());
+	$('.sdhigh').text(getNow());
+	$('.sdlow').text(getNow());
+};
 
 navigator.geolocation.getCurrentPosition(function(location) {
 	var lat = location.coords.latitude,
@@ -12,10 +20,19 @@ navigator.geolocation.getCurrentPosition(function(location) {
 		type: 'GET',
 		dataType: 'jsonp',
 		success: function(data) {
+			var currentTemp = data.currently.temperature;
+			return currentTemp;
 			console.log(data.currently.temperature);
+			console.log(data.currently.apparentTemperatureMin);
+			console.log(data.currently.apparentTemperatureMax);
 		},
 		error: function(){
 			console.log(arguments[1]);
 		}
 	});
+});
+
+$(document).ready( function() {
+	yourWeather();
+	sdWeather();
 });
